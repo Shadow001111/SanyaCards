65cfe4fac7d8b6b2600138015385b4cd1ae86c33
Shadow001111 <121736709+Shadow001111@users.noreply.github.com>
2024-12-01 12:27:05 +0200

    Ready for patch

--- SanyaCards/SanyaCards.cs
+++ SanyaCards/SanyaCards.cs
@@ -27,7 +27,7 @@ namespace SanyaCards
     {
         private const string ModId = "com.Shadow.SanyaCards";
         private const string ModName = "SanyaCards";
-        public const string Version = "0.10.1";
+        public const string Version = "0.10.2";
         public const string ModInitials = "SANYA";
 
         public static SanyaCards instance { get; private set; }
